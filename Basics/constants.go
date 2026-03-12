3c1d8fdb4a137d08fe26f45561f7e209642dc19c
Burhaan Ashraf <burhaanashraf@outlook.com>
2026-03-12 19:13:26 +0530

    learned about constants

---
+++ Basics/constants.go
@@ -0,0 +1,19 @@
+package main
+
+const PI = 3.14
+const GRAVITY = 9.81
+
+func constants() {
+	const day int = 365
+
+	const ( 
+	// this is how we can initialize multiple constants --> this is called const block which is used to group similar consts together
+		monday = 1
+		tuesday = 2
+		months = 12
+		friday = 5
+		saturday int = 6
+	)
+
+
+}
